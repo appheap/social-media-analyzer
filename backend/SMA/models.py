745be462944f5f70006f2c23738b1773245558fc
Soran Ghadri <kpg.kurd@gmail.com>
2020-09-07 11:55:47 +0100

    1. Creating serializers.py 2. test model used as the first trial

--- backend/SMA/models.py
+++ backend/SMA/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class TestModel(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
