673621f558ddc10f13d75068c1e983f42c01e30e
Soran Ghadri <kpg.kurd@gmail.com>
2020-09-07 09:06:44 +0100

    1. Corrected manage.py : uncommented try/except block

--- backend/manage.py
+++ backend/manage.py
@@ -8,15 +8,15 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_analyzer.settings')
     application = get_wsgi_application()
-    # try:
-    #     from django.core.management import execute_from_command_line
-    # except ImportError as exc:
-    #     raise ImportError(
-    #         "Couldn't import Django. Are you sure it's installed and "
-    #         "available on your PYTHONPATH environment variable? Did you "
-    #         "forget to activate a virtual environment?"
-    #     ) from exc
-    # execute_from_command_line(sys.argv)
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
