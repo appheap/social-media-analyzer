b3b0849fc6d000db004f5f964e2ed0eb8230bb0d
sigma1326 <taleb.zarhesh@gmail.com>
2020-12-14 18:50:21 +0330

    + Update `User` type of telegram app + Update `Restriction` type of telegram app + Add `UserManager` class to `User` model in telegram app + Add `UserQuerySet` class to `User` model in telegram app + Add `RestrictionManager` class to `User` model in telegram app + Add `RestrictionQuerySet` class to `User` model in telegram app

--- backend/telegram/models/users_and_chats/restriction.py
+++ backend/telegram/models/users_and_chats/restriction.py
@@ -1,5 +1,115 @@
+from django.db import DatabaseError
 from django.db import models
+
+from pyrogram import types
+from telegram import models as tg_models
+from telegram.globals import logger
 from ..base import BaseModel
+from typing import List
+
+
+class RestrictionQuerySet(models.QuerySet):
+    def update_or_create_restriction(self, **kwargs):
+        try:
+            return self.update_or_create(
+                **kwargs
+            )
+        except DatabaseError as e:
+            logger.exception(e)
+        except Exception as e:
+            logger.exception(e)
+
+        return None
+
+    def clear_restrictions(
+            self,
+            *,
+            user: tg_models.User = None,
+            chat: tg_models.Chat = None,
+            message: tg_models.Message = None
+    ):
+        try:
+            self.filter(
+                user=user,
+                chat=chat,
+                message=message
+            ).delete()
+        except DatabaseError as e:
+            logger.exception(e)
+        except Exception as e:
+            logger.exception(e)
+
+
+class RestrictionManager(models.Manager):
+    def get_queryset(self) -> "RestrictionQuerySet":
+        return RestrictionQuerySet(self.model, using=self._db)
+
+    def update_or_create(
+            self,
+            *,
+            restriction: types.Restriction,
+            user: tg_models.User = None,
+            chat: tg_models.Chat = None,
+            message: tg_models.Message = None
+    ):
+        if restriction is None:
+            return None
+        parsed_object = RestrictionManager._parse_restriction(restriction)
+        if not parsed_object:
+            return None
+
+        self.get_queryset().clear_restrictions(user=user, chat=chat, message=message)
+        return self.get_queryset().update_or_create_restriction(
+            **parsed_object
+        )
+
+    def bulk_create_restrictions(
+            self,
+            *,
+            restrictions: List['types.Restriction'],
+            user: tg_models.User = None,
+            chat: tg_models.Chat = None,
+            message: tg_models.Message = None
+    ):
+        if restrictions is None:
+            return
+        self.get_queryset().clear_restrictions(user=user, chat=chat, message=message)
+
+        try:
+            self.get_queryset() \
+                .bulk_create(filter(lambda obj: obj is not None, map(RestrictionManager._parse, restrictions)))
+        except DatabaseError as e:
+            logger.exception(e)
+        except Exception as e:
+            logger.exception(e)
+
+    @staticmethod
+    def _parse(restriction):
+        parsed_object = RestrictionManager._parse_restriction(restriction)
+        if not parsed_object:
+            return None
+        obj = Restriction(
+            **parsed_object
+        )
+        return obj
+
+    @staticmethod
+    def _parse_restriction(
+            restriction: types.Restriction,
+            user: tg_models.User = None,
+            chat: tg_models.Chat = None,
+            message: tg_models.Message = None):
+
+        if restriction is None:
+            return None
+        return {
+            'platform': restriction.platform,
+            'reason': restriction.reason,
+            'text': restriction.text,
+            'user': user,
+            'chat': chat,
+            'message': message,
+        }
 
 
 class Restriction(BaseModel):
@@ -7,14 +117,9 @@ class Restriction(BaseModel):
     The reason why this chat/bot might be unavailable to some users. This field is available only in case is_restricted of `chat` or `bot` is True.
     """
 
-    id = models.CharField(max_length=256, primary_key=True, )  # `chat|user:_id`
-
     platform = models.CharField(max_length=256, null=True, blank=True)
     reason = models.CharField(max_length=256, null=True, blank=True)
     text = models.CharField(max_length=256, null=True, blank=True)
-
-    class Meta:
-        unique_together = [('id', 'platform', 'reason', 'text'), ]
 
     chat = models.ForeignKey(
         'telegram.Chat',
@@ -29,9 +134,17 @@ class Restriction(BaseModel):
         null=True, blank=True,
         related_name='restrictions',
     )
+
+    message = models.ForeignKey(
+        'telegram.Message',
+        on_delete=models.CASCADE,
+        null=True, blank=True,
+        related_name='restrictions',
+    )
+
+    objects = RestrictionManager()
+
     ##############################################
-    is_deleted = models.BooleanField(default=False, null=False)
-    deleted_at = models.BigIntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.reason
