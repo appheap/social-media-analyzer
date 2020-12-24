32cb4e37ad25e33a967f438d619549ec263d1335
sigma1326 <taleb.zarhesh@gmail.com>
2020-12-25 00:50:45 +0330

    + Update `AdminLogAnalyzerMetaData` model in telegram app + Update `ChatMemberCountAnalyzerMetaData` model in telegram app + Update `ChatMembersAnalyzerMetaData` model in telegram app + Update `ChatSharedMediaAnalyzerMetaData` model in telegram app + Update `MessageViewsAnalyzerMetaData` model in telegram app

--- backend/telegram/models/analyzers/admin_log_analyzer.py
+++ backend/telegram/models/analyzers/admin_log_analyzer.py
@@ -9,13 +9,13 @@ class AdminLogAnalyzerMetaData(BaseModel):
     enabled = models.BooleanField(default=False)
     first_analyzed_ts = models.BigIntegerField(null=True, blank=True)
     last_analyzed_ts = models.BigIntegerField(null=True, blank=True)
-    disable_date_ts = models.BigIntegerField(null=True, blank=True)
+    disable_ts = models.BigIntegerField(null=True, blank=True)
     disable_reason = models.TextField(max_length=256, null=True, blank=True)
 
     ######################################
 
     # current active telegram channel
-    telegram_channel = models.OneToOneField(
+    telegram_channel = models.OneToOneField(  # fixme: what's the usage?
         'telegram.TelegramChannel',
         on_delete=models.CASCADE,
         related_name='admin_log_analyzer_metadata',
--- backend/telegram/models/analyzers/message_views_analyzer.py
+++ backend/telegram/models/analyzers/message_views_analyzer.py
@@ -4,13 +4,13 @@ from ..base import BaseModel
 
 
 class ChatMessageViewsAnalyzerMetaData(BaseModel):
-    id = models.CharField(max_length=256, primary_key=True)  # `chat_id:created_at`
+    id = models.BigIntegerField(primary_key=True)  # `chat__chat_id`
 
-    enabled = models.BooleanField()
-    first_analyzed_at = models.BigIntegerField(null=True, blank=True)
-    last_analyzed_at = models.BigIntegerField(null=True, blank=True)
-    disabled_at = models.BigIntegerField(null=True, blank=True)
-    disable_reason = models.CharField(max_length=256, null=True, blank=True, )
+    enabled = models.BooleanField(default=False)
+    first_analyzed_ts = models.BigIntegerField(null=True, blank=True)
+    last_analyzed_ts = models.BigIntegerField(null=True, blank=True)
+    disable_ts = models.BigIntegerField(null=True, blank=True)
+    disable_reason = models.TextField(max_length=256, null=True, blank=True, )
 
     ######################################
     # `chat` : chat this analyzer metadata belongs to
