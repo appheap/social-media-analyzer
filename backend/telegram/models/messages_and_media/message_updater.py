30af50d62b6cdd195a01043cdb94c7dbd3864f17
sigma1326 <taleb.zarhesh@gmail.com>
2020-12-24 16:29:45 +0330

    + Update `Message` model of telegram app + Add `MessageUpdater` class to telegram app

---
+++ backend/telegram/models/messages_and_media/message_updater.py
@@ -0,0 +1,43 @@
+from pyrogram import types
+from django.db import models
+from .message import Message
+from telegram import models as tg_models
+
+
+class MessageUpdater:
+
+    @staticmethod
+    def update_or_create_message_from_raw(
+            *,
+            model: models.Model,
+            field_name: str,
+            raw_message: types.Message,
+            chat_id: int,
+            logger_account: "tg_models.TelegramAccount" = None
+    ):
+        field = getattr(model, field_name, None)
+        if field and not isinstance(field, Message):
+            return
+
+        if field:
+            if raw_message:
+                field.update_fields_from_raw(
+                    raw_message=raw_message,
+                    chat_id=chat_id,
+                    logger_account=logger_account
+                )
+            else:
+                setattr(model, field_name, None)
+                model.save(model)
+        else:
+            if raw_message:
+                setattr(
+                    model,
+                    field_name,
+                    Message.objects.update_or_create_from_raw(
+                        raw_user=raw_message,
+                        chat_id=chat_id,
+                        logger_account=logger_account
+                    )
+                )
+                model.save(model)
