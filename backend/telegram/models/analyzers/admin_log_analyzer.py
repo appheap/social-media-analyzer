d422d5e2fc9ebe93bb97edd264bb1ff576c20b90
sigma1326 <taleb.zarhesh@gmail.com>
2020-12-25 00:41:59 +0330

    + Update `AdminLogAnalyzerMetaData` model in telegram app

--- backend/telegram/models/analyzers/admin_log_analyzer.py
+++ backend/telegram/models/analyzers/admin_log_analyzer.py
@@ -4,13 +4,13 @@ from ..base import BaseModel
 
 
 class AdminLogAnalyzerMetaData(BaseModel):
-    id = models.CharField(max_length=256, primary_key=True)  # `chat__chat_id`
+    id = models.BigIntegerField(primary_key=True)  # `chat__chat_id`
 
-    enabled = models.BooleanField()
+    enabled = models.BooleanField(default=False)
     first_analyzed_ts = models.BigIntegerField(null=True, blank=True)
     last_analyzed_ts = models.BigIntegerField(null=True, blank=True)
     disable_date_ts = models.BigIntegerField(null=True, blank=True)
-    disable_reason = models.CharField(max_length=256, null=True, blank=True, )
+    disable_reason = models.TextField(max_length=256, null=True, blank=True)
 
     ######################################
 
