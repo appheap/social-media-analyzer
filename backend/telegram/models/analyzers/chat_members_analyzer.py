ca5cae7e2dc175aaeee42f9ddb3d209571e35ae1
sigma1326 <taleb.zarhesh@gmail.com>
2020-12-25 00:50:45 +0330

    + Update `AdminLogAnalyzerMetaData` model in telegram app + Update `ChatMemberCountAnalyzerMetaData` model in telegram app + Update `ChatMembersAnalyzerMetaData` model in telegram app + Update `ChatSharedMediaAnalyzerMetaData` model in telegram app + Update `MessageViewsAnalyzerMetaData` model in telegram app

--- backend/telegram/models/analyzers/chat_members_analyzer.py
+++ backend/telegram/models/analyzers/chat_members_analyzer.py
@@ -4,17 +4,17 @@ from ..base import BaseModel
 
 
 class ChatMembersAnalyzerMetaData(BaseModel):
-    id = models.CharField(max_length=256, primary_key=True)  # `chat_id:created_at`
+    id = models.BigIntegerField(primary_key=True)  # `chat__chat_id`
 
-    enabled = models.BooleanField()
-    first_analyzed_at = models.BigIntegerField(null=True, blank=True)
-    last_analyzed_at = models.BigIntegerField(null=True, blank=True)
-    disabled_at = models.BigIntegerField(null=True, blank=True)
-    disable_reason = models.CharField(max_length=256, null=True, blank=True, )
+    enabled = models.BooleanField(default=False)
+    first_analyzed_ts = models.BigIntegerField(null=True, blank=True)
+    last_analyzed_ts = models.BigIntegerField(null=True, blank=True)
+    disable_ts = models.BigIntegerField(null=True, blank=True)
+    disable_reason = models.TextField(max_length=256, null=True, blank=True)
 
     ######################################
     # current active telegram channel
-    telegram_channel = models.OneToOneField(
+    telegram_channel = models.OneToOneField(  # fixme: what's the usage?
         'telegram.TelegramChannel',
         on_delete=models.SET_NULL,
         related_name='chat_member_analyzer_metadata',
