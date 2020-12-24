1668ce12133bb6bafd0f1e5e362d57567272f626
sigma1326 <taleb.zarhesh@gmail.com>
2020-12-24 23:26:53 +0330

    + Rename `search_counter` module to `get_search_counters` in pyrogram package + Add `SearchCounter` model to pyrogram package + Add `parse_search_counters` function to `utils` module in pyrogram package
    + Update `ChatSharedMedia` model in telegram app
    + Add `ChatSharedMediaQuerySet` class to `chat_shared_media` module in telegram app
    + Add `ChatSharedMediaManager` class to `chat_shared_media` module in telegram app

--- backend/telegram/models/analyzers/chat_shared_media.py
+++ backend/telegram/models/analyzers/chat_shared_media.py
@@ -1,7 +1,71 @@
-from django.db import models
-import arrow
+from typing import Optional, List
 
+from django.db import models, DatabaseError
+import arrow
+from pyrogram import types
+from telegram import models as tg_models
 from ..base import BaseModel
+from telegram.globals import logger
+
+_filter_names = (
+    'photo',
+    'video',
+    'document',
+    'music',
+    'voice',
+    'url',
+    'video_note',
+    'animation',
+    'location',
+    'contact',
+)
+
+
+class ChatSharedMediaQuerySet(models.QuerySet):
+    def update_or_create_shared_media(self, **kwargs) -> Optional["ChatSharedMedia"]:
+        try:
+            return self.update_or_create(
+                **kwargs
+            )[0]
+        except DatabaseError as e:
+            logger.exception(e)
+        except Exception as e:
+            logger.exception(e)
+
+        return None
+
+
+class ChatShardMediaManager(models.Manager):
+    def get_queryset(self) -> ChatSharedMediaQuerySet:
+        return ChatSharedMediaQuerySet(self.model, using=self._db)
+
+    def update_or_create_shared_media_from_raw(
+            self,
+            *,
+            raw_search_counters: List['types.SearchCounter'],
+            db_chat: 'tg_models.Chat',
+            logger_account: 'tg_models.TelegramAccount',
+            date_ts: int,
+    ) -> Optional['ChatSharedMedia']:
+
+        if raw_search_counters is None or not len(raw_search_counters) or db_chat is None or date_ts is None:
+            return None
+
+        kwargs = {}
+        for raw_search_counter in raw_search_counters:
+            if raw_search_counter.filter_name in _filter_names:
+                kwargs[raw_search_counter.filter_name] = raw_search_counter.count
+            else:
+                raise ValueError(f'Invalid filter name : {raw_search_counter.filter_name}')
+
+        return self.get_queryset().update_or_create_shared_media(
+            **{
+                **kwargs,
+                'id': f'{db_chat.chat_id}:{date_ts}',
+                'chat': db_chat,
+                'logged_by': logger_account,
+            }
+        )
 
 
 class ChatSharedMedia(BaseModel):
@@ -38,6 +102,8 @@ class ChatSharedMedia(BaseModel):
         null=True, blank=True,
         related_name='shared_media_history',
     )
+
+    objects = ChatShardMediaManager()
 
     class Meta:
         ordering = ('-date_ts',)
