02d600d9b65539e98e5642c9c1e9ab1df5bb533a
sigma1326 <taleb.zarhesh@gmail.com>
2020-12-25 13:12:13 +0330

    + Rename `ChannelParticipant` model to `ChatMember` in telegram app + Rename `ChannelParticipantTypes` model to `ChatMemberTypes` in telegram app
    + Update `ChatMember` model in telegram app
    + Add `ChatMemberQuerySet` class to `chat_member` module in telegram app
    + Add `ChatMemberManager` class to `chat_member` module in telegram app
    
    Signed-off-by: sigma <taleb.zarhesh>

--- backend/telegram/models/users_and_chats/__init__.py
+++ backend/telegram/models/users_and_chats/__init__.py
@@ -1,6 +1,10 @@
 from .membership import Membership
-from .chat_member import ChannelParticipantTypes
-from .chat_member import ChannelParticipant
+
+from .chat_member import ChatMember
+from .chat_member import ChatMemberTypes
+from .chat_member import ChatMemberQuerySet
+from .chat_member import ChatMemberManager
+
 from .restriction import Restriction
 from .restriction import RestrictionManager
 from .restriction import RestrictionQuerySet
@@ -11,8 +15,12 @@ __all__ = [
     "Membership",
     "AdminShip",
     "Role",
-    "ChannelParticipant",
-    "ChannelParticipantTypes",
+
+    "ChatMember",
+    "ChatMemberTypes",
+    "ChatMemberQuerySet",
+    "ChatMemberManager",
+
     "Restriction",
     "RestrictionManager",
     "RestrictionQuerySet",
