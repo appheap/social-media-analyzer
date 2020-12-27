485baf915587f14c85d2f598d13de836ef472715
sigma1326 <taleb.zarhesh@gmail.com>
2020-12-27 14:35:49 +0330

    + Update `ChatMember` type of pyrogram package

--- backend/pyrogram/types/chats/chat_member.py
+++ backend/pyrogram/types/chats/chat_member.py
@@ -21,7 +21,7 @@ class ChatMember(Object):
             is_current_user: bool = None,
             promoted_by: "types.User" = None,
 
-            is_member: bool = None,
+            has_left: bool = None,
             kicked_by: "types.User" = None,
             banned_rights: "types.ChatPermissions" = None,
     ):
@@ -37,7 +37,7 @@ class ChatMember(Object):
         self.is_current_user = is_current_user
         self.promoted_by = promoted_by
 
-        self.is_member = is_member
+        self.has_left = has_left
         self.kicked_by = kicked_by
         self.banned_rights = banned_rights
 
@@ -62,9 +62,9 @@ class ChatMember(Object):
         elif isinstance(participant, (raw.types.ChatParticipantAdmin, raw.types.ChannelParticipantAdmin)):
             status = 'administrator'
         elif isinstance(participant, raw.types.ChannelParticipantBanned):
-            status = "kicked" if participant.banned_rights.view_messages else "restricted",
+            status = "kicked" if participant.banned_rights.view_messages else "restricted"
 
-        return ChatMember(
+        chat_member = ChatMember(
             client=client,
 
             user=types.User._parse(client, users.get(getattr(participant, 'user_id', None), None)),
@@ -79,8 +79,14 @@ class ChatMember(Object):
             promoted_by=types.User._parse(client, users.get(participant.promoted_by, None)) if getattr(
                 participant, 'promoted_by', None) else None,
 
-            is_member=getattr(participant, 'left', None),
+            has_left=getattr(participant, 'left', None),
             kicked_by=types.User._parse(client, users.get(participant.kicked_by, None)) if getattr(
                 participant, 'kicked_by', None) else None,
             banned_rights=types.ChatPermissions._parse(getattr(participant, 'banned_rights', None)),
         )
+
+        if chat_member.status == 'member':
+            if chat_member.join_date is None and chat_member.has_left is None:
+                chat_member.status = 'user'
+
+        return chat_member
