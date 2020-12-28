143d58bb424ccf9e4fb36654388640f654626d92
sigma1326 <taleb.zarhesh@gmail.com>
2020-12-28 13:55:31 +0330

    + Update `TelegramAccount` model in telegram app + Add `TelegramAccountQuerySet` class from `telegram_account` module in telegram app + Add `TelegramAccountManager` class from `telegram_account` module in telegram app

--- backend/telegram/models/base/__init__.py
+++ backend/telegram/models/base/__init__.py
@@ -1,10 +1,18 @@
 from .base_model import BaseModel
+
 from .telegram_account import TelegramAccount
+from .telegram_account import TelegramAccountQuerySet
+from .telegram_account import TelegramAccountManager
+
 from .telegram_channel import TelegramChannel
 
 __all__ = [
     "BaseModel",
+
     "TelegramAccount",
+    "TelegramAccountQuerySet",
+    "TelegramAccountManager",
+
     "TelegramChannel",
 
 ]
--- backend/telegram/models/base/telegram_account.py
+++ backend/telegram/models/base/telegram_account.py
@@ -1,10 +1,97 @@
-from django.db import models
+from typing import Optional
+
+from django.db import models, DatabaseError
 
 # this model is used as proxy to control access,etc to the objects they refer to
 from ..base import BaseModel
+from db.models import SoftDeletableBaseModel
+from db.models import SoftDeletableQS
+from telegram.globals import logger
+from pyrogram import types
+import pyrogram
+from users import models as site_models
+from telegram import models as tg_models
 
 
-class TelegramAccount(BaseModel):
+class TelegramAccountQuerySet(SoftDeletableQS):
+    def update_or_create_account(self, **kwargs) -> Optional['TelegramAccount']:
+        try:
+            return self.update_or_create(
+                **kwargs
+            )[0]
+        except DatabaseError as e:
+            logger.exception(e)
+        except Exception as e:
+            logger.exception(e)
+
+        return None
+
+    def get_by_user_id(self, user_id: int) -> Optional['TelegramAccount']:
+        try:
+            return self.get(user_id=user_id)
+        except TelegramAccount.DoesNotExist as e:
+            pass
+        except DatabaseError as e:
+            logger.exception(e)
+        except Exception as e:
+            logger.exception(e)
+
+        return None
+
+
+class TelegramAccountManager(models.Manager):
+    def get_queryset(self) -> TelegramAccountQuerySet:
+        return TelegramAccountQuerySet(self.model, using=self._db)
+
+    def update_or_create_from_raw(
+            self,
+            *,
+            db_site_user: 'site_models.SiteUser',
+            db_user: 'tg_models.User',
+            client: pyrogram.Client,
+
+    ) -> Optional['TelegramAccount']:
+
+        if db_site_user is None or db_user is None or client is None:
+            return None
+
+        parsed_object = self._parse(db_user=db_user)
+        if len(parsed_object):
+            db_account = self.get_queryset().update_or_create_account(
+                **{
+                    **parsed_object,
+                    'site_user': db_site_user,
+                    'telegram_user': db_user,
+                    'api_id': client.api_id,
+                    'api_hash': client.api_hash,
+                    'session_name': client.session_name,
+                }
+            )
+            return db_account
+
+        return None
+
+    @staticmethod
+    def _parse(*, db_user: 'tg_models.User'):
+        if db_user is None:
+            return {}
+
+        return {
+            'user_id', db_user.user_id,
+            'username', db_user.username,
+            'first_name', db_user.first_name,
+            'last_name', db_user.last_name,
+            'is_bot', db_user.is_bot,
+            'is_restricted', db_user.is_restricted,
+            'is_scam', db_user.is_scam,
+            'is_verified', db_user.is_verified,
+            'phone_number', db_user.phone_number,
+            'dc_id', db_user.dc_id,
+            'language_code', db_user.language_code,
+        }
+
+
+class TelegramAccount(BaseModel, SoftDeletableBaseModel):
     user_id = models.BigIntegerField(primary_key=True)
     username = models.CharField(max_length=32, null=True, blank=True)
     first_name = models.CharField(max_length=64, null=True, blank=True)
@@ -21,9 +108,6 @@ class TelegramAccount(BaseModel):
     api_id = models.CharField(max_length=256, null=True, blank=True)
     api_hash = models.CharField(max_length=256, null=True, blank=True)
     session_name = models.CharField(max_length=256, null=True, blank=True)
-
-    is_deleted = models.BooleanField(default=False)
-    deleted_at = models.BigIntegerField(null=True, blank=True)
 
     # `telegram_channels` : telegram channels added by this account
     # `chats` : chats added by this account
@@ -58,6 +142,8 @@ class TelegramAccount(BaseModel):
         null=True, blank=True,
         related_name='telegram_account',
     )
+
+    accounts = TelegramAccountManager()
 
     def __str__(self):
         return str(self.username if self.username else "") + str(self.first_name if self.first_name else "") + str(
