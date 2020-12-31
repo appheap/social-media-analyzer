aab622cca742d202d2835ae44da2a88c28093fc9
sigma1326 <taleb.zarhesh@gmail.com>
2020-12-31 14:01:23 +0330

    + Update `input_message_content` package in `pyrogram/types` to layer 122

--- backend/pyrogram/types/input_message_content/input_text_message_content.py
+++ backend/pyrogram/types/input_message_content/input_text_message_content.py
@@ -1,4 +1,22 @@
-from typing import Union
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-2020 Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Optional
 
 from pyrogram import raw
 from pyrogram.parser import Parser
@@ -23,7 +41,7 @@ class InputTextMessageContent(InputMessageContent):
             Disables link previews for links in this message.
     """
 
-    def __init__(self, message_text: str, parse_mode: Union[str, None] = object, disable_web_page_preview: bool = None):
+    def __init__(self, message_text: str, parse_mode: Optional[str] = object, disable_web_page_preview: bool = None):
         super().__init__()
 
         self.message_text = message_text
