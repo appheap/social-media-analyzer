a0e981a761ff8066ebfe9643ee26bb20bf819d29
sigma1326 <taleb.zarhesh@gmail.com>
2021-01-03 11:51:42 +0330

    + Add `update_membership_status` module to `telegram/methods/users_and_chats/` package

--- backend/telegram/methods/__init__.py
+++ backend/telegram/methods/__init__.py
@@ -1,12 +1,14 @@
 from .analyzers import Analyzers
 from .base import Base
 from .chats import Chats
+from .users_and_chats import UsersAndChats
 
 
 class TelegramMethods(
     Analyzers,
     Base,
     Chats,
+    UsersAndChats,
 
 ):
     pass
---
+++ backend/telegram/methods/users_and_chats/__init__.py
@@ -0,0 +1,8 @@
+from .update_membership_status import UpdateMembershipStatus
+
+
+class UsersAndChats(
+    UpdateMembershipStatus,
+
+):
+    pass
