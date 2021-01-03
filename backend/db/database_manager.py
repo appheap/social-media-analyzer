bc690574b13a2e7302b8469e1387e9911a3b2038
sigma1326 <taleb.zarhesh@gmail.com>
2021-01-03 16:07:53 +0330

    + Update `database_manager` module in `db` package + Update `scaffold` module in `db` package

--- backend/db/database_manager.py
+++ backend/db/database_manager.py
@@ -1,8 +1,13 @@
 from .methods import TelegramMethods
-from .scaffold import Scaffold
 
 
-class DataBaseManager(Scaffold):
+class DataBaseManager:
 
     def __int__(self):
+        from telegram import models as tg_models
+        from users import models as site_models
+
+        self.tg_models = tg_models
+        self.site_models = site_models
+
         self.telegram = TelegramMethods()
--- backend/db/scaffold.py
+++ backend/db/scaffold.py
@@ -1,7 +1,5 @@
-class Scaffold:
-    def __init__(self):
-        from telegram import models as tg_models
-        from users import models as site_models
+from .database_manager import DataBaseManager
 
-        self.tg_models = tg_models
-        self.site_models = site_models
+
+class Scaffold(DataBaseManager):
+    pass
