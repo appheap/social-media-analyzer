18003b2fa78074b7bc56be2712097c758323c452
sigma1326 <taleb.zarhesh@gmail.com>
2021-01-09 13:30:56 +0330

    + Update `telegram_consumer_manager` module in `telegram/client/` package

--- backend/telegram/client/telegram_consumer_manager.py
+++ backend/telegram/client/telegram_consumer_manager.py
@@ -1,6 +1,11 @@
 from threading import Thread
 from typing import List
+
+from kombu import Connection
+
 import pyrogram
+from core.globals import logger
+from .worker import Worker
 
 
 class TelegramConsumerManager(Thread):
@@ -14,4 +19,5 @@ class TelegramConsumerManager(Thread):
         self.index = index
 
     def run(self) -> None:
-        pass
+        logger.info(f"Telegram Consumer {self.index} started ....")
+        Worker(connection=Connection('amqp://localhost'), clients=self.clients, index=self.index).run()
