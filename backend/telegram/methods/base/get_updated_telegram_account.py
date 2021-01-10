8953d4b645edff2c8aaee703925fef8bcde67c57
sigma1326 <taleb.zarhesh@gmail.com>
2021-01-10 16:18:29 +0330

    + Update `get_updated_telegram_account` module `telegram/methods/base/`

--- backend/telegram/methods/base/get_updated_telegram_account.py
+++ backend/telegram/methods/base/get_updated_telegram_account.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from db.scaffold import Scaffold
 import pyrogram
+from pyrogram import types
 from telegram import models as tg_models
 from users import models as site_models
 
@@ -11,9 +12,16 @@ class GetUpdatedTelegramAccount(Scaffold):
     def get_updated_telegram_account(
             self,
             db_site_user: 'site_models.SiteUser',
-            db_user: 'tg_models.User',
-            client: pyrogram.Client,
+            raw_user: 'types.User',
+            client: 'pyrogram.Client',
     ) -> Optional['tg_models.TelegramAccount']:
+        if db_site_user is None or raw_user is None or client is None:
+            return None
+
+        db_user = self.get_updated_user(
+            raw_user=raw_user
+        )
+
         return self.tg_models.TelegramAccount.accounts.update_or_create_from_raw(
             db_site_user=db_site_user,
             db_user=db_user,
