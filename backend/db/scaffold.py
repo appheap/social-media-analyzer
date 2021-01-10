26e4210f0f4749fdf812507d1e1f1214d1cf2efd
sigma1326 <taleb.zarhesh@gmail.com>
2021-01-10 16:17:34 +0330

    + Update `scaffold` module in `db` package

--- backend/db/scaffold.py
+++ backend/db/scaffold.py
@@ -1,5 +1,20 @@
-from .database_manager import DataBaseManager
+from pyrogram import types
+from typing import Optional
+from telegram import models as tg_models
+from users import models as site_models
 
 
-class Scaffold(DataBaseManager):
-    pass
+class Scaffold:
+
+    def __init__(self):
+        self.tg_models = tg_models
+        self.site_models = site_models
+
+    def get_updated_user(
+            self,
+            *,
+            raw_user: 'types.User',
+
+            db_message_view: 'tg_models.MessageView' = None
+    ) -> Optional['tg_models.User']:
+        pass
