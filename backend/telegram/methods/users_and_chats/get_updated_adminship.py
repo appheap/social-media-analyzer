883beb56b5478e0d9a188e07cc95e5b2ac351de5
sigma1326 <taleb.zarhesh@gmail.com>
2021-01-13 21:21:20 +0330

    + Update `get_updated_adminship` module in `telegram/methods/users_and_chats/` package

--- backend/telegram/methods/users_and_chats/get_updated_adminship.py
+++ backend/telegram/methods/users_and_chats/get_updated_adminship.py
@@ -1,7 +1,5 @@
 from typing import Optional
 
-from django.db import transaction
-
 from db.scaffold import Scaffold
 from pyrogram import types
 from telegram import models as tg_models
@@ -19,21 +17,8 @@ class GetUpdatedAdminShip(Scaffold):
         if db_account is None or db_chat is None or raw_chat is None:
             return None
 
-        with transaction.atomic():
-            db_adminship = self.tg_models.AdminShip.objects.update_or_create_from_raw(
-                db_account=db_account,
-                db_chat=db_chat,
-                raw_chat=raw_chat,
-            )
-            db_adminship.update_or_create_chat_admin_rights_from_raw(
-                model=db_adminship,
-                field_name='admin_rights',
-                raw_chat_admin_rights=raw_chat.admin_rights
-            )
-            db_adminship.update_or_create_chat_permissions_from_raw_obj(
-                model=db_adminship,
-                field_name='banned_rights',
-                raw_chat_permissions=raw_chat.chat_permissions
-            )
-
-            return db_adminship
+        return self.tg_models.AdminShip.objects.update_or_create_from_raw(
+            db_account=db_account,
+            db_chat=db_chat,
+            raw_chat=raw_chat,
+        )
