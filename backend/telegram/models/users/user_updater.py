e7eb1735d181ce6037fd832a78f58e8bf0f6f7c0
sigma1326 <taleb.zarhesh@gmail.com>
2021-01-13 23:58:51 +0330

    + Update `UserUpdater` class in telegram models

--- backend/telegram/models/users/user_updater.py
+++ backend/telegram/models/users/user_updater.py
@@ -4,6 +4,7 @@ from django.db import models
 
 from pyrogram import types
 from .user import User
+from telegram import models as tg_models
 
 
 class UserUpdater:
@@ -33,5 +34,31 @@ class UserUpdater:
                     User.users.update_or_create_from_raw(
                         raw_user=raw_user
                     )
+                )
+                model.save()
+
+    @staticmethod
+    def update_or_create_user_from_db_user(
+            *,
+            model: models.Model,
+            field_name: str,
+            db_user: 'tg_models.User'
+    ):
+        field = getattr(model, field_name, None)
+        if field and not isinstance(field, User):
+            return
+
+        if field:
+            if db_user:
+                setattr(model, field_name, db_user)
+            else:
+                setattr(model, field_name, None)
+                model.save()
+        else:
+            if db_user:
+                setattr(
+                    model,
+                    field_name,
+                    db_user
                 )
                 model.save()
