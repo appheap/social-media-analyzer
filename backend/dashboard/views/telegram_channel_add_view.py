44126719d2909691fb6c951b3efecc427cd24785
sigma1326 <taleb.zarhesh@gmail.com>
2021-01-24 21:33:54 +0330

    + Update `telegram_channel_add_view` module in `dashboard` app

--- backend/dashboard/views/telegram_channel_add_view.py
+++ backend/dashboard/views/telegram_channel_add_view.py
@@ -1,11 +1,10 @@
 import json
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core import exceptions
 from django.views.generic import FormView
 
+from core.global_db import db
 from telegram import forms as tg_forms
-from telegram import models as tg_models
 from telegram import tasks
 from .json_response_mixin import JsonResponseFormMixin
 
@@ -24,28 +23,26 @@ class TelegramChannelAddView(LoginRequiredMixin, JsonResponseFormMixin, FormView
         return super().form_invalid(form)
 
     def form_valid(self, form):
-        # logger.info(form.instance.__dict__)
         form.instance.user = self.request.user
 
-        try:
-            tg_models.AddChannelRequest.objects.get(done=False,
-                                                    channel_username=str(form.instance.channel_username).lower(),
-                                                    site_user=self.request.user, )
-        except exceptions.ObjectDoesNotExist as e:
-            response = json.loads(
-                tasks.request_add_tg_channel(
-                    channel_username=form.instance.channel_username,
-                    db_tg_account_admin_id=form.instance.admin.pk,
-                    db_userid=self.request.user.pk,
-                )
-            )
-            self.extra_data.update(response)
-
-            if not response['success']:
-                form.add_error(None, response['message'])
-                return super().form_invalid(form)
-        else:
+        if db.telegram.undone_add_channel_request_exists(
+                db_site_user=self.request.user,
+                channel_username=str(form.instance.channel_username).lower()
+        ):
             form.add_error(None, 'You have made a request for this channel already.')
+            return super().form_invalid(form)
+
+        response = json.loads(
+            tasks.request_add_tg_channel(
+                channel_username=form.instance.channel_username,
+                db_tg_account_admin_id=form.instance.admin.pk,
+                db_userid=self.request.user.pk,
+            )
+        )
+        self.extra_data.update(response)
+
+        if not response['success']:
+            form.add_error(None, response['message'])
             return super().form_invalid(form)
 
         return super().form_valid(form)
@@ -53,5 +50,5 @@ class TelegramChannelAddView(LoginRequiredMixin, JsonResponseFormMixin, FormView
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        context['telegram_accounts'] = tg_models.TelegramAccount.objects.all()
+        context['telegram_accounts'] = db.telegram.get_all_available_telegram_accounts()
         return context
