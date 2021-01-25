771b2d2cbaafdcd86953f8e902d6e3cd4f41d209
sigma1326 <taleb.zarhesh@gmail.com>
2021-01-25 23:58:18 +0330

    + Update `accounts_view` module of `dashboard` app

--- backend/dashboard/views/accounts_view.py
+++ backend/dashboard/views/accounts_view.py
@@ -17,4 +17,13 @@ class AccountsView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        db_telegram_channels = db.telegram.get_user_telegram_channels(
+            db_site_user=self.request.user
+        )
+        context['tg_channels'] = db_telegram_channels
+        db_profile_photos = [db.telegram.get_latest_profile_photo(db_chat=db_channel.chat) for db_channel in
+                             db_telegram_channels]
+        context['profile_photos'] = db_profile_photos
+        context['objects'] = list(zip(db_telegram_channels, db_profile_photos))
+
         return context
