b442909be26d22ddb218a5309548c9d8162db44d
sigma1326 <taleb.zarhesh@gmail.com>
2021-02-05 00:54:25 +0330

    + Update `TelegramChannel` model of `telegram` app

--- backend/telegram/models/base/telegram_channel.py
+++ backend/telegram/models/base/telegram_channel.py
@@ -28,6 +28,32 @@ class TelegramChannelQuerySet(SoftDeletableQS):
 
         return None
 
+    def create_channel(self, **kwargs) -> Optional['TelegramChannel']:
+        try:
+            return self.create(
+                **kwargs
+            )
+        except DatabaseError as e:
+            logger.exception(e)
+        except Exception as e:
+            logger.exception(e)
+
+        return None
+
+    def update_channel(self, **kwargs) -> 'bool':
+        try:
+            return bool(
+                self.update(
+                    **kwargs
+                )
+            )
+        except DatabaseError as e:
+            logger.exception(e)
+        except Exception as e:
+            logger.exception(e)
+
+        return False
+
     def get_by_channel_id(self, channel_id: int) -> Optional['TelegramChannel']:
         try:
             return self.get(channel_id=channel_id)
@@ -48,6 +74,12 @@ class TelegramChannelQuerySet(SoftDeletableQS):
     def filter_by_channel_username(self, *, username: 'str') -> 'TelegramChannelQuerySet':
         return self.filter(username=username)
 
+    def filter_by_channel_id(self, *, channel_id: 'int') -> 'TelegramChannelQuerySet':
+        return self.filter(channel_id=channel_id)
+
+    def filter_by_id(self, *, id: 'str') -> 'TelegramChannelQuerySet':
+        return self.filter(id=id)
+
 
 class TelegramChannelManager(models.Manager):
     def get_queryset(self) -> TelegramChannelQuerySet:
@@ -66,9 +98,24 @@ class TelegramChannelManager(models.Manager):
             return None
 
         return self.get_queryset() \
+            .not_deleted() \
             .filter_by_site_user(db_site_user=db_site_user) \
             .filter_by_channel_username(username=channel_username) \
             .exists()
+
+    def get_telegram_channel_by_site_user_and_channel_id(
+            self,
+            *,
+            db_site_user: 'site_models.SiteUser',
+            channel_id: int,
+    ) -> Optional['TelegramChannel']:
+        if db_site_user is None or channel_id is None:
+            return None
+        return self.get_queryset() \
+            .not_deleted() \
+            .filter_by_site_user(db_site_user=db_site_user) \
+            .filter_by_channel_id(channel_id=channel_id) \
+            .first()
 
     def get_user_telegram_channels(
             self,
@@ -76,43 +123,46 @@ class TelegramChannelManager(models.Manager):
     ) -> 'TelegramChannelQuerySet':
         return self.get_queryset().filter_by_site_user(db_site_user=db_site_user)
 
-    def update_or_create_from_raw(
+    def create_channel_from_raw(
             self,
             *,
             raw_chat: types.Chat,
             db_site_user: 'site_models.SiteUser',
             db_account: 'tg_models.TelegramAccount',
             db_chat: 'tg_models.Chat',
-
-            create: bool = True,
     ) -> Optional['TelegramChannel']:
-
-        if raw_chat is None or db_account is None or db_chat is None:
+        if raw_chat is None or db_site_user is None or db_account is None or db_chat is None:
             return None
-
-        if create and db_site_user is None:
-            raise ValueError('`db_site_user` cannot be None')
 
         parsed_object = self._parse(raw_chat=raw_chat)
         if len(parsed_object):
-            if create:
-                parsed_object.update(
-                    {
-                        'site_user': db_site_user,
-                    }
-                )
-            db_channel = self.get_queryset().update_or_create_channel(
-                channel_id=raw_chat.id,
-                defaults={
+            db_channel = self.get_queryset().create_channel(
+                **{
                     **parsed_object,
                     'chat': db_chat,
-                    'telegram_account': db_account
+                    'telegram_account': db_account,
+                    'site_user': db_site_user,
                 }
             )
-
             return db_channel
-
         return None
+
+    def update_channel_from_raw(
+            self,
+            *,
+            id: str,
+            raw_chat: types.Chat,
+    ) -> 'bool':
+
+        if id is None or raw_chat is None:
+            return False
+
+        parsed_object = self._parse(raw_chat=raw_chat)
+        if len(parsed_object):
+            return self.get_queryset().filter_by_id(id=id).update_channel(
+                **parsed_object,
+            )
+        return False
 
     @staticmethod
     def _parse(*, raw_chat: types.Chat) -> dict:
@@ -120,6 +170,7 @@ class TelegramChannelManager(models.Manager):
             return {}
 
         return {
+            'channel_id': raw_chat.id,
             'is_account_creator': raw_chat.channel.is_creator,
             'is_account_admin': raw_chat.is_admin,
             'username': raw_chat.channel.username.lower() if getattr(raw_chat.channel, 'username', None) else None,
@@ -190,3 +241,9 @@ class TelegramChannel(BaseModel, SoftDeletableBaseModel):
 
     def get_absolute_url(self):
         return reverse('dashboard:accounts')
+
+    def update_fields_from_raw(self, *, raw_chat: 'types.Chat') -> 'bool':
+        return TelegramChannel.channels.update_channel_from_raw(
+            id=self.id,
+            raw_chat=raw_chat
+        )
